8ceeba0cb629125a7bfa8d631a84c1ae2af41706
Sayan Pramanik <111434702+DeepForest2002@users.noreply.github.com>
2025-04-02 11:46:53 +0000

    change has been made

---
+++ basics/variable.js
@@ -0,0 +1,11 @@
+const account_id=4598;
+let account_email='pramaniksayan406@gmail.com';
+var account_city="kolkata";
+account_age=19;
+
+//account_id=8978; //This is not allowed because it is const 
+account_email="hfhgh@gmail.com";
+account_city="Bengaluru";
+account_age=56;
+
+console.table([account_id,account_city,account_email,account_age]);
