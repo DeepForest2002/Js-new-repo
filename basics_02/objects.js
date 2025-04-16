5f2d6bc0c00f334039b2d179974ec8d815ce0f3a
Sayan Pramanik <111434702+DeepForest2002@users.noreply.github.com>
2025-04-16 12:24:11 +0000

    Objects added

---
+++ basics_02/objects.js
@@ -0,0 +1,40 @@
+
+//What is Objects is js -> Object is a non primitive data type in js where we store value in the form of kay and value pair 
+
+
+const mysym= Symbol("Key1")
+
+const object={
+    name:"Sayan Pramnaik",
+    Age:18,
+    Email:"pramnaiksayan@google.com",
+    isLoggedIn: false,
+    LastLogIn: ["Monday","Sunday"],
+    "Salary":150000, //This type of values can not be accesssed using . operators
+
+    //Adding a symbol in the object 
+    [mysym]:"KeyVal1"
+}
+
+//How to access object values 
+// console.log(object.Age)
+
+//Using [] operator
+// console.log(object["name"])
+
+// console.log(object["Salary"])
+//Accessing the symbol value 
+// console.log(object[mysym])
+
+// console.log(object)
+
+// Freeze the js Object so that is can not be manipulated 
+// Object.freeze(object) 
+
+//Adding function to a object
+object.func=function(){
+    console.log(`Hello,my name is ${this.name}`)
+}
+object.func()
+
+
---
+++ basics_02/objects_02.js
@@ -0,0 +1,35 @@
+// Objects Part 2
+// Singleton Objects in js
+
+const newObj= new Object() 
+
+const regularUser={
+    name:{
+        fullName:{
+            firstName:"Sayan",
+            LastName: "Pramanik"
+        }
+    }
+}
+console.log(regularUser.name.fullName.firstName)
+
+//Combining many objects as one object
+const obj1={1:'a',2:'b'}
+const obj2={3:'a',4:'b'}
+const obj3={5:'a',6:'b'}
+
+//By normal methods 
+// const obj4={obj1,obj2} 
+
+// By Assign method  
+const obj4=Object.assign({},obj1,obj2,obj3) //Assign all the values in the empty object 
+// console.log(obj4) 
+
+//By spread Operator 
+const obj5={...obj1,...obj2, ...obj3}
+// console.log(obj5)
+
+console.log(Object.entries(regularUser))
+
+//To check wheather an object contains a ceratin property or not 
+console.log(Object.hasOwnProperty("name"))
