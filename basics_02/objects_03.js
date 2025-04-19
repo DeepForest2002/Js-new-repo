2544df3b1dde0aee55fc79af29871af22c002ecb
Sayan Pramanik <111434702+DeepForest2002@users.noreply.github.com>
2025-04-19 17:31:16 +0000

    Json  Format

---
+++ basics_02/objects_03.js
@@ -0,0 +1,13 @@
+//Destructuring objects is js 
+const Course={
+    name:"C++",
+    CourseInstructor:"Hitesh"
+}
+
+const {CourseInstructor}=Course
+console.log(CourseInstructor)
+
+//Structure of JSON file
+// {
+//     "Name":"Sayan" //Here key and its value is in string format
+// }
