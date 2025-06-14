c364486531b48591117ded8bb305aedf449cca2f
Sayan Pramanik <111434702+DeepForest2002@users.noreply.github.com>
2025-06-14 06:33:22 +0000

    Promisre and api request added

---
+++ Advance/promises.js
@@ -0,0 +1,66 @@
+//Prmoises in js
+const promise1 = new Promise(function (resolve, reject) {
+  //Do an async task
+  // Db calls, cryptography, network
+
+  setTimeout(function () {
+    console.log("Task is complete");
+    resolve();
+  }, 1000);
+});
+
+promise1.then(function () {
+  console.log("Promise consumed");
+});
+
+//Method number 2
+
+new Promise(function (resoleve, reject) {
+  setTimeout(function () {
+    console.log("Async task 2 is completed");
+    resoleve();
+  }, 2000);
+}).then(function () {
+  console.log("Promise 2 is resolved");
+});
+
+//Third promise (Then and resolve has connections)
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(function () {
+    resolve({
+      userName: "Sayan Pramanik",
+      email: "ramaniksayan406@gmail.com",
+    });
+  }, 3000);
+});
+promise3.then(function (user) {
+  console.log(user);
+});
+
+//Method 4
+
+const promise4 = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = false;
+    if (error === false) {
+      resolve({ name: "Sayan Pramanik", email: "GFGFG@gmail.com" });
+    } else {
+      reject("Error: Something happened");
+    }
+  }, 4000);
+});
+
+promise4
+  .then((user) => {
+    console.log(user);
+    return user.name;
+  })
+  .then((userName) => {
+    console.log(userName);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(() => {
+    console.log("Finally,The promise is either resolved or rejected");
+  });
